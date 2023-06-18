498bcffec51644177e4bc33b4698d817318b578c
Rikki Martin <rikkibmartin@gmail.com>
2023-06-18 02:34:28 -0500

    fully functional

--- checkuseronline.py
+++ checkuseronline.py
@@ -4,18 +4,23 @@ import requests
 
 webhook_url = 'https://discord.com/api/webhooks/1119547326302793768/stcriwZkOBcVQlzGTwTpw415XPRirVkBdoE0b9sZF9u3Zi9n_QNG1RnLgTLbddVakq6v'
 
+#Currently loading SKOTN
+owner_url = "https://inventory.roblox.com/v2/assets/439945661/owners"
+
 # List of all user_ids
-user_ids = [1752117, 25013944]
+# user_ids = [1752117, 25013944] deprecated for testing reasons
+user_ids = []
 
 # Dict to check previous state
 my_dict = {}
 
 # Infinite loop ran to continuously check all the players' presence in the user_ids list
 def monitor_players():
+    print("Starting online checker...")
     while True:
         #print("Loop")
         check_presence()
-        time.sleep(10)
+        time.sleep(30)
 
 def check_presence():
     headers = {
@@ -132,7 +137,42 @@ def send_message(user_id, place_id):
     if response.status_code != 204:
         print(f"Failed to send Discord message. Error code: {response.status_code}")
 
+def load_owners():
+    ownerHeader = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Cookie': '.ROBLOSECURITY=_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_B5B9BCFFDC07B97884ADB97803A43BBABF22858BBD53FFEB2C87D0A330A0F9216073ADA0C4BA01CE17CB84A261DE621853C4809561B55D201F0D303D5615B7CF034B35A99189B0E7CA1E31581EE571E237E692E97BBB0520C8DF293F16AEA3ED6796B3024591DB3EA3746829E137D76F9AD2BB90B1AA2246BB94F39112790AB2C8FF74D6E5A2C3261C90539F35377C018FC27427624546FE409E2037E4515CE746635B3F9F7EF2BB6073905417E057E0BC00F3EE5F75715234A4DDB0566AF73A8F181417FC8308F2F01B729CCDEB676EE7BB04D22E8F0D3B06D96D667C7723D90873A0B7677860210E0E3E37FAB4E121CB7B7A71C0F6E8C2A2297958EBBD197B578170C152174E2C144D0F31AF222D4504B9189F4CB06D7D2165EC46809694C565591C7E38D68C3EDC2DB2C1F5E0155B1AF343A914F91C52C7A2018D4FAE9903CDA06A218830CD6106EE5EAD674F78E00146196786F5C09ED8207018C5E381E5CA50DA33C03E81247334F90B5C7245B9DC21734E'
+    }
+
+    response = requests.get(owner_url, headers=ownerHeader, params={"limit": 100, "sortOrder": "Asc"})
+    data = response.json()
+
+    for item in data["data"]:
+        if item is not None and item.get("owner") is not None and item.get("updated") is not None:
+            if item["updated"] < "2021-01-01T00:00:00.000Z":
+                user_ids.append(item["owner"]["id"])
+
+    next_page_cursor = data.get("nextPageCursor")
+
+    while next_page_cursor:
+        params = {"limit": 100, "sortOrder": "Asc", "cursor": next_page_cursor}
+        response = requests.get(owner_url, headers=ownerHeader, params=params)
+        data = response.json()
+
+        for item in data["data"]:
+            if item is not None and item.get("owner") is not None and item.get("updated") is not None:
+                if item["updated"] < "2021-01-01T00:00:00.000Z":
+                    user_ids.append(item["owner"]["id"])
+
+        next_page_cursor = data["nextPageCursor"]
+        
+        message_one = "Loaded -> "
+        owner_count = len(user_ids)
+        message_two = " <- lim owners into list"
+        result = message_one + str(owner_count) + message_two
+        print(result)
 
 
 
+load_owners()
 monitor_players()
