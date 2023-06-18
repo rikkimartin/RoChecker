799d95d5c7b38a4ca243d96dc69ff1ba3b8c4d00
Rikki Martin <rikkibmartin@gmail.com>
2023-06-18 05:28:30 -0500

    changes and fixes

--- checkuseronline.py
+++ checkuseronline.py
@@ -8,7 +8,6 @@ webhook_url = 'https://discord.com/api/webhooks/1119547326302793768/stcriwZkOBcV
 owner_url = "https://inventory.roblox.com/v2/assets/439945661/owners"
 
 # List of all user_ids
-# user_ids = [1752117, 25013944] deprecated for testing reasons
 user_ids = []
 
 # Dict to check previous state
@@ -16,13 +15,34 @@ my_dict = {}
 
 # Infinite loop ran to continuously check all the players' presence in the user_ids list
 def monitor_players():
-    print("Starting online checker...")
+    isSplit = False
     while True:
-        #print("Loop")
-        check_presence()
-        time.sleep(30)
+        counter = 0
+        success = 0
+        print("Starting online checker...")
+        if isSplit == False:
+            split_length = len(user_ids) // 30 #MAX REQUEST FOR CHECK PRESENCE IS 200 PER REQUEST
+            split_list = [user_ids[i:i+split_length] for i in range(0, len(user_ids), split_length)]
+            isSplit = True
+        for part in split_list:
+            print("Length of list being checked: ", len(part))
+            print("List count: ", counter)
+            success_check = check_presence(part)
+            if success_check:
+                success += 1
+            print("===============")
+            time.sleep(10)
+            counter += 1
+        
+        print("Total success: ", success, "out of 30")
+        print("\t Restarting online checker...")
+        time.sleep(5)
 
-def check_presence():
+        # To test post errors only run once and exit()
+        exit()
+
+# Checks every user_id in the list if they are in-game
+def check_presence(owner_list_id):
     headers = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
@@ -30,8 +50,10 @@ def check_presence():
     }
 
     data = {
-        "userIds": user_ids
+        "userIds": owner_list_id
     }
+
+    my_dict = {num: 0 for num in owner_list_id}
 
     response = requests.post('https://presence.roblox.com/v1/presence/users', headers=headers, data=json.dumps(data))
     if response.status_code == 200:
@@ -42,16 +64,18 @@ def check_presence():
                 isOnline = presence.get("userPresenceType") 
                 userID = presence.get("userId")
                 placeID = presence.get("placeId")
-                print(userID)
-                print(placeID)
                 previous_state = my_dict.get(userID) 
                 if previous_state != 2 and isOnline == 2:
                     send_message(userID,placeID)
                 my_dict[userID] = isOnline
+            print(">>SUCCESS<<")
+            return True
         else:
             print("No presence data found")
     else:
-        print("Post request failed.")
+        print(f"Post request failed. Status code: {response.status_code}")
+        return False
+    
 
 # Retrieves a player's roblox username through RobloxAPI, with user_id as param
 def get_roblox_username(user_id):
@@ -96,38 +120,95 @@ def get_roblox_profile_url(user_id):
 def get_rolimons_profile_url(user_id):
     return f"https://www.rolimons.com/player/{user_id}"
 
+# Retrieves the place_id the user is in if they are in-game
 def get_place_id(placeID):
-    if placeID > 0:
-        return f"https://www.roblox.com/games/{placeID}/"
-    else:
+    if placeID is not None: 
+        return f"[Click here](https://www.roblox.com/games/{placeID}/)"
+    elif placeID is None:
         return "Follows are off"
-        
+
+# Checks if a user has their follows on or off
+def check_follow_status(placeID):
+    if placeID is not None:
+        return "On"
+    elif placeID is None:
+        return "Off"
+
+# Checks if a user has premium or not 
+def get_premium_status(user_id):
+    url = f"https://premiumfeatures.roblox.com/v1/users/{user_id}/validate-membership"
+
+    headers = {
+        'Cookie': '.ROBLOSECURITY=_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_B5B9BCFFDC07B97884ADB97803A43BBABF22858BBD53FFEB2C87D0A330A0F9216073ADA0C4BA01CE17CB84A261DE621853C4809561B55D201F0D303D5615B7CF034B35A99189B0E7CA1E31581EE571E237E692E97BBB0520C8DF293F16AEA3ED6796B3024591DB3EA3746829E137D76F9AD2BB90B1AA2246BB94F39112790AB2C8FF74D6E5A2C3261C90539F35377C018FC27427624546FE409E2037E4515CE746635B3F9F7EF2BB6073905417E057E0BC00F3EE5F75715234A4DDB0566AF73A8F181417FC8308F2F01B729CCDEB676EE7BB04D22E8F0D3B06D96D667C7723D90873A0B7677860210E0E3E37FAB4E121CB7B7A71C0F6E8C2A2297958EBBD197B578170C152174E2C144D0F31AF222D4504B9189F4CB06D7D2165EC46809694C565591C7E38D68C3EDC2DB2C1F5E0155B1AF343A914F91C52C7A2018D4FAE9903CDA06A218830CD6106EE5EAD674F78E00146196786F5C09ED8207018C5E381E5CA50DA33C03E81247334F90B5C7245B9DC21734E'
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        if data == True:
+            return "True"
+        else:
+            return "False"
+
+# Calculates the user's total RAP
+def get_total_rap(user_id):
+    total_rap = 0
+    url = f'https://inventory.roblox.com/v1/users/{user_id}/assets/collectibles?limit=100&sortOrder=Asc'
+    response = requests.get(url)
+
+    while response.status_code == 200:
+        data = response.json()
+
+        total_rap += sum(asset['recentAveragePrice'] for asset in data['data'])
+
+        if 'nextPageCursor' in data and data['nextPageCursor'] is not None:
+            next_cursor = data['nextPageCursor']
+
+            url = f'https://inventory.roblox.com/v1/users/{user_id}/assets/collectibles?limit=100&sortOrder=Asc&cursor={next_cursor}'
+
+            response = requests.get(url)
+        else:
+            break
+    
+    return total_rap
 
 # Embeds a message that is send through the discord webhook with all of a player's information. Communicates through a post request on DiscordAPI webhook url.
 def send_message(user_id, place_id):
     username = get_roblox_username(user_id)
     data = {
     "embeds": [{
-        "title": "A limited owner is in-game!",
+        "title": "Profile Link",
+        "url": get_roblox_profile_url(user_id),
         "color": 14548992,
-        "description": f"**{username}** is now in-game! Check out their profile:",
+        "description": f"**{username}** is now in-game!",
         "image": {
             "url": get_avatar_image(user_id)
         },
         "fields": [
             {
-                "name": "Roblox Profile",
-                "value": get_roblox_profile_url(user_id),
+                "name": "Rolimons Profile:",
+                "value": f'[Click here]({get_rolimons_profile_url(user_id)})',
                 "inline": True
             },
             {
-                "name": "Rolimons Profile",
-                "value": get_rolimons_profile_url(user_id),
-                "inline": True
-            },
-            {
-                "name": "Game Link",
+                "name": "Game Link:",
                 "value": get_place_id(place_id),
+                "inline": True
+            },
+            {
+                "name": "Premium:",
+                "value": get_premium_status(user_id),
+                "inline": True
+            },
+            {
+                "name": "Follows:",
+                "value": check_follow_status(place_id),
+                "inline": True
+            },
+            {
+                "name": "RAP:",
+                "value": get_total_rap(user_id),
                 "inline": True
             }
         ]
@@ -137,6 +218,7 @@ def send_message(user_id, place_id):
     if response.status_code != 204:
         print(f"Failed to send Discord message. Error code: {response.status_code}")
 
+# Loads all owners user IDs before 2021 into the user_id list
 def load_owners():
     ownerHeader = {
     'Content-Type': 'application/json',
@@ -176,3 +258,5 @@ def load_owners():
 
 load_owners()
 monitor_players()
+
+
